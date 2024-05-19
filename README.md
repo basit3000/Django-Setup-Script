133703883868e87d94940799cb0f33adce7bf891
Muhammad Basit Zaheer <basit3000@yahoo.com>
2024-05-19 14:36:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Django Setup Script
+
+The purpose of this script is to automate the creation of a Python virtual environment, set up a Django server, upgrade pip, and start the server.
+
+## Prerequisites
+
+- Ensure Python is installed locally. You can download it from [python.org](https://www.python.org/).
+
+## Steps to Run the Script
+
+1. **Open Command Prompt / Shell / PowerShell**
+
+    Open your preferred command line interface.
+
+2. **Clone this Project**
+
+    Clone the repository to your local machine using the following command:
+
+    ```sh
+    git clone <repository-url>
+
+3. **Navigate to the project directory**
+
+    cd <project_directory>
+
+4. **Run the script**
+
+    python script.py
---
+++ script.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+
+def run_command(command, **kwargs):
+    result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True, **kwargs)
+    print(result.stdout)
+    if result.stderr:
+        print(result.stderr)
+    return result
+
+try:
+    print("Upgrading pip...")
+    run_command('python -m pip install -U pip')
+
+    print("Creating virtual environment...")
+    run_command('python -m venv venv')
+    venv_python = os.path.join('venv', 'Scripts', 'python.exe')
+
+    print("Installing Django in virtual environment...")
+    run_command('{} -m pip install django'.format(venv_python))
+
+    project_name = input("Please enter the name of the project.")
+
+    print("Creating Django project...")
+    run_command('{} -m django startproject {}'.format(venv_python, project_name))
+
+    print("Running Django development server...")
+    subprocess.run('{} newsite/manage.py runserver'.format(venv_python), shell=True, check=True)
+
+except subprocess.CalledProcessError as e:
+    print(f"An error occurred while running a command: {e}")
+    print(e.stderr)
